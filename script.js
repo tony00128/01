68ae481250064caf0fa7bd34dfcbb9cc25986734
tony00128 <tony.chang30105@gmail.com>
2024-07-04 11:04:27 +0800

    second

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let num = document.getElementById('change');
 
 btn.addEventListener('click', () => {　 // 監聽此元素是否被點擊，若被點擊則執行內容
     alert("點到我了");                     // 跳出「點到我了」視窗                  // 設為禁止點擊
-    btn.style.color = "red";
+    // btn.style.color = "red";
     let randnum = Math.random();
     num.textContent = randnum;
 });
